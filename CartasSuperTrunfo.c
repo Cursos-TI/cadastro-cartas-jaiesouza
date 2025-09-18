ed4ebcd394cb308c898cdea50b37ffc94fbb45eb
jaiesouza <jairsouza1213@gmail.com>
2025-09-18 11:53:12 +0000

    alteração2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,17 +10,20 @@ char estado1[] = "A";
 char codigo1 [50] = "A01";
 char nome1 [50] = "São paulo";
 int populacao1 = 12325000;
-float area1 = 1521.2;
+float area1 = 1521.11;
 float pib1 = 699.28;
-
+int numero1 = 50;
   // Área para entrada de dados
-printf("carta 1:\n");
+printf("Carta 1: \n");
 printf("Estado: %s \n", estado1);
 printf("Código: %s \n", codigo1);
 printf("Nome da cidade: %s \n", nome1);
 printf("Populaçã0: %d \n", populacao1);
-printf("Área: %f \n", area1,);
-printf("PIB: %f \n", pib1,);
+printf("Área: %2.f", area1);
+printf(" km2 \n");
+printf("PIB: %2.f", pib1);
+printf(" bilhões de reais \n");
+printf ("Numero de pontos turisticos: %d \n", numero1);
   // Área para exibição dos dados da cidade
 
 return 0;
